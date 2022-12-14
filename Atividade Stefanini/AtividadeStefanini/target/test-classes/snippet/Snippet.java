b4cd75a4a1975d9038b129858056a895d8a0c24a
Bruno621 <62024860+Bruno621@users.noreply.github.com>
2022-12-14 18:03:27 -0300

    Delete Atividade Stefanini/AtividadeStefanini directory

--- Atividade Stefanini/AtividadeStefanini/target/test-classes/snippet/Snippet.java
+++
@@ -1,9 +0,0 @@
-package snippet;
-
-public class Snippet {
-	public static void main(String[] args) {
-		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-				driver = new ChromeDriver();
-	}
-}
-
